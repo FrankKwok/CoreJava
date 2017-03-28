941fb3aa2c7ba68e67c6457575c86099fe432a84
Frank Kwok <ghlfrankkwok@gmail.com>
2017-03-28 18:43:46 +0800

    Create Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CoreJava
 Core Java 10th Edition Code
+
+This project doesn't include GUI and Applet part.
---
+++ src/main/java/com/github/frankkwok/corejava/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Core Java 10th Edition:
+ * Volume I-Fundamentals
+ * Volume II-Advanced Features
+ *
+ * @author Frank Kwok on 2017/3/28.
+ */
+package com.github.frankkwok.corejava;
