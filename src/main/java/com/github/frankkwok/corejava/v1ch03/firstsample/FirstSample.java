9d28c09b7f6ada30513bb63141d6ad269696b343
Frank Kwok <ghlfrankkwok@gmail.com>
2017-03-28 19:01:07 +0800

    v1ch03: FirstSample

---
+++ src/main/java/com/github/frankkwok/corejava/v1ch03/firstsample/FirstSample.java
@@ -0,0 +1,12 @@
+package com.github.frankkwok.corejava.v1ch03.firstsample;
+
+/**
+ * First sample in Volume I Chapter 3
+ *
+ * @author Frank Kwok on 2017/3/28.
+ */
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'hello, world'");
+    }
+}
---
+++ src/main/java/com/github/frankkwok/corejava/v1ch03/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Volume I Chapter 3: Fundamental Programming Structures in Java
+ *
+ * @author Frank Kwok on 2017/3/28.
+ */
+package com.github.frankkwok.corejava.v1ch03;
