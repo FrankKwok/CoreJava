5d6c216a62ba2d7473f324bd9f7f29d2f58d553d
Frank Kwok <ghlfrankkwok@gmail.com>
2017-03-30 11:49:31 +0800

    v1ch06: Lambda Expressions

---
+++ src/main/java/com/github/frankkwok/corejava/v1ch06/lambda/LambdaTest.java
@@ -0,0 +1,34 @@
+package com.github.frankkwok.corejava.v1ch06.lambda;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * lambda expressions
+ *
+ * @author Frank Kwok on 2017/3/30.
+ */
+public class LambdaTest {
+    public static void main(String[] args) {
+        String[] planets = new String[]{
+                "Mercury", "Venus", "Earth",
+                "Mars", "Jupiter", "Saturn",
+                "Uranus", "Neptune"
+        };
+
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sort in dictionary order:");
+        Arrays.sort(planets);
+        System.out.println("Sort by length: ");
+        System.out.println(Arrays.toString(planets));
+        Arrays.sort(planets, (first, second) -> Integer.compare(first.length(), second.length()));
+
+
+        Timer timer = new Timer(3000, event -> System.out.println("The time is " + new Date()));
+        timer.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
