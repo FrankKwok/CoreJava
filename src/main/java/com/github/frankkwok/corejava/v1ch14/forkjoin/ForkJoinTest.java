ef7f717e287bfcc03f52f3bd8392b8405537e6de
Frank Kwok <ghlfrankkwok@gmail.com>
2017-04-16 22:58:45 +0800

    v1ch14: Fork Join

---
+++ src/main/java/com/github/frankkwok/corejava/v1ch14/forkjoin/ForkJoinTest.java
@@ -0,0 +1,56 @@
+package com.github.frankkwok.corejava.v1ch14.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.function.DoublePredicate;
+
+/**
+ * @author Frank Kwok on 2017/4/16.
+ */
+public class ForkJoinTest {
+    public static void main(String[] args) {
+        final int SIZE = 10000000;
+        double[] numbers = new double[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            numbers[i] = Math.random();
+        }
+        Counter counter = new Counter(numbers, 0, numbers.length, x -> x > 0.5);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(counter);
+        System.out.println(counter.join());
+    }
+}
+
+class Counter extends RecursiveTask<Integer> {
+    private static final int THRESHOLD = 1000;
+    private double[] values;
+    private int from;
+    private int to;
+    private DoublePredicate filter;
+
+    Counter(double[] values, int from, int to, DoublePredicate filter) {
+        this.values = values;
+        this.from = from;
+        this.to = to;
+        this.filter = filter;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (to - from < THRESHOLD) {
+            int count = 0;
+            for (int i = from; i < to; i++) {
+                if (filter.test(values[i])) {
+                    count++;
+                }
+            }
+            return count;
+        } else {
+            int mid = (from + to) / 2;
+            Counter first = new Counter(values, from, mid, filter);
+            Counter second = new Counter(values, mid, to, filter);
+            invokeAll(first, second);
+            return first.join() + second.join();
+        }
+    }
+}
